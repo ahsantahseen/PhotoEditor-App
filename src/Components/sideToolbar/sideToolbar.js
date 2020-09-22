f2a22536538ef7499ea30451134efeb18d573192
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-09-22 16:32:14 +0500

    Styling Done Toolbar Styling Done Working on the controls

--- src/Components/sideToolbar/sideToolbar.js
+++ src/Components/sideToolbar/sideToolbar.js
@@ -1,9 +1,15 @@
 import React from "react";
-
 import classes from "../../Styling/App.module.css";
+import SideToolbarItem from "./sideToolbarItem";
 
 const SideToolbar = () => {
-  return <div className={classes.sideToolbar}>Side Toolbar </div>;
+  return (
+    <div className={classes.sideToolbar}>
+      <SideToolbarItem></SideToolbarItem>
+      <SideToolbarItem></SideToolbarItem>
+      <SideToolbarItem></SideToolbarItem>
+    </div>
+  );
 };
 
 export default SideToolbar;
---
+++ src/Components/sideToolbar/sideToolbarItem.js
@@ -0,0 +1,8 @@
+import classes from "../../Styling/App.module.css";
+import React from "react";
+
+const SideToolbarItem = () => {
+  return <button className={classes.sideToolbarItem}>Button Item    </button>;
+};
+
+export default SideToolbarItem;
